2490a28d0d416ed95dcd8089e820f77ddf084967
Christwide Oscar <triple101.games@gmail.com>
2022-02-18 12:59:32 -0600

    Completed Projects

--- api/project/model.js
+++ api/project/model.js
@@ -1,11 +1,51 @@
-// build your `Project` model here
-const db = require('../../data/dbConfig');
+const db = require('../../data/dbConfig')
 
-async function getProjects(){
-    const projectRows = await db('projects as p')
-    return projectRows;
+const getProjects = async () => {
+    const projects = await db('projects')
+
+    return projects.map(project => {
+        if(project.project_completed === 1) {
+            return {
+                ...project,
+                project_completed: true
+            }
+    } else {
+        return {
+            ...project,
+            project_completed: false
+        }
+    }
+  })
 }
 
+// const getProjectById = (project_id) => {
+//     return db('projects').where('project_id', project_id).first()
+// }
+const getProjectById = async (project_id) => {
+    const project = await db('projects').where('project_id', project_id).first()
+
+    if(project.project_completed === 1) {
+        return {
+            ...project,
+            project_completed: true
+        }
+    } else {
+        return {
+            ...project,
+            project_completed: false
+        }
+    }
+}
+
+async function createProject(project) {
+    const [project_id] = await db('projects').insert(project)
+    return getProjectById(project_id)
+}
+
+
+
 module.exports = {
-    getProjects
-};
+    getProjects,
+    getProjectById,
+    createProject
+}
--- api/project/router.js
+++ api/project/router.js
@@ -1,27 +1,35 @@
 // build your `/api/projects` router here
-const express = require('express');
-const router = express.Router()
-const Project = require('./model')
+const router = require('express').Router()
+const { checkProjectId, checkProjectPayload } = require('./middleware')
+const Projects = require('./model')
 
-router.get('/', (req,res,next)=>{
-  Project.getProjects()
-    .then(projects => {
-      if(!projects){
-        res.status(200).json([])}
-      else{
-        res.status(200).json(projects)
-      }
-    })
-    .catch(next)
+router.get('/', async (req, res, next) => {
+    try {
+        const projects = await Projects.getProjects()
+        res.json(projects)
+    } catch(err) {
+        next(err)
+    }
 })
 
- // eslint-disable-next-line no-unused-vars
- router.use((err, req, res, next)=>{
-     res.status(500).json({
-         customMessage: "Something inside the project router went wrong",
-         message: err.message,
-         stack: err.stack,
-     })
- })
+router.get('/:id', checkProjectId, (req, res) => {
+    res.json(req.project)
+})
 
- module.exports = router;
+router.post('/', checkProjectPayload, async (req, res, next) => {
+    try {
+        const newProject = await Projects.createProject(req.body);
+        res.status(201).json(newProject)
+    } catch(err) {
+        next(err)
+    }
+})
+
+router.use((err, req, res, next) => { //eslint-disable-line
+    res.status(err.status || 500).json({
+        message: err.message,
+        stack: err.stack
+    })
+})
+
+module.exports = router;
