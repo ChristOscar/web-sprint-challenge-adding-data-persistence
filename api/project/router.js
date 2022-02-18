c090b976b1a4f79e6e0a39e05e9de45defe4bc69
Christwide Oscar <triple101.games@gmail.com>
2022-02-18 08:43:55 -0600

    Add basic router function / error catcher

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,18 @@
 // build your `/api/projects` router here
+const express = require('express');
+ const router = express.Router()
+
+ router.use('*', (req, res)=>{
+     res.json({api:'up'})
+ })
+
+ // eslint-disable-next-line no-unused-vars
+ router.use((err, req, res, next)=>{
+     res.status(500).json({
+         customMessage: "Something inside the recipes router went wrong",
+         message: err.message,
+         stack: err.stack,
+     })
+ })
+
+ module.exports = router;
