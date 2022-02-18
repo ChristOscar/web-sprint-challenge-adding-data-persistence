ad0ae4a79e9ff80d86d7b12dbb65e6f0c92a455c
Christwide Oscar <triple101.games@gmail.com>
2022-02-18 11:59:56 -0600

    Project router made

--- api/project/router.js
+++ api/project/router.js
@@ -1,6 +1,19 @@
 // build your `/api/projects` router here
 const express = require('express');
  const router = express.Router()
+ const Project = require('./model')
+
+ router.get('/projects', (req,res,next)=>{
+    Project.getProjects()
+      .then(projects => {
+        if(!projects){
+          res.status(200).json([])}
+        else{
+          res.status(200).json(projects)
+        }
+      })
+      .catch(next)
+  })
 
  router.use('*', (req, res)=>{
      res.json({api:'up'})
