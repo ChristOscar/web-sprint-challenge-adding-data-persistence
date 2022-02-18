3d8eb3ec4099d3817546c91e0ad811fd4ddb8959
Christwide Oscar <triple101.games@gmail.com>
2022-02-18 12:06:23 -0600

    projects router and model work

--- api/project/model.js
+++ api/project/model.js
@@ -1,11 +1,11 @@
 // build your `Project` model here
 const db = require('../../data/dbConfig');
 
-async function getProject(){
+async function getProjects(){
     const projectRows = await db('projects as p')
     return projectRows
 }
 
 module.exports = {
-    getProject
+    getProjects
 };
--- api/project/router.js
+++ api/project/router.js
@@ -1,28 +1,24 @@
 // build your `/api/projects` router here
 const express = require('express');
- const router = express.Router()
- const Project = require('./model')
+const router = express.Router()
+const Project = require('./model')
 
- router.get('/projects', (req,res,next)=>{
-    Project.getProjects()
-      .then(projects => {
-        if(!projects){
-          res.status(200).json([])}
-        else{
-          res.status(200).json(projects)
-        }
-      })
-      .catch(next)
-  })
-
- router.use('*', (req, res)=>{
-     res.json({api:'up'})
- })
+router.get('/', (req,res,next)=>{
+  Project.getProjects()
+    .then(projects => {
+      if(!projects){
+        res.status(200).json([])}
+      else{
+        res.status(200).json(projects)
+      }
+    })
+    .catch(next)
+})
 
  // eslint-disable-next-line no-unused-vars
  router.use((err, req, res, next)=>{
      res.status(500).json({
-         customMessage: "Something inside the recipes router went wrong",
+         customMessage: "Something inside the project router went wrong",
          message: err.message,
          stack: err.stack,
      })
